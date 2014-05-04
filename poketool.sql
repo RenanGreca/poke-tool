7910e2ba9d9f66bc6c6fe4bb881e5aa7487e6384
Mari Bennett <bennett.mari@gmail.com>
2014-05-04 13:35:49 -0700

    Added to-pokedex php; added some small test data for showing something in register.

--- poketool.sql
+++ poketool.sql
@@ -1,40 +1,42 @@
 DROP DATABASE IF EXISTS poketool;
 CREATE DATABASE poketool;
 
+GRANT ALL PRIVILEGES ON poketool.* TO 'test'@'localhost'; #test passcode is test;
+
 USE poketool;
 
-CREATE TABLE Pokemon (
+CREATE TABLE Pokemon(
     dexno INTEGER NOT NULL,
     name CHAR(20) NOT NULL,
     type1 INTEGER NOT NULL,
     type2 INTEGER,
-    PRIMARY KEY dexno
+    PRIMARY KEY (dexno)
+);
+
+CREATE TABLE Users(
+    uid INTEGER NOT NULL,
+    name VARCHAR(16) NOT NULL,
+    PRIMARY KEY (uid)
 );
 
 CREATE TABLE Pokedex(
     dexno INTEGER NOT NULL,
-    name VARCHAR(16) NOT NULL,
     game VARCHAR(16) NOT NULL,
     uid INTEGER NOT NULL,
     PRIMARY KEY (uid, dexno),
     CONSTRAINT FOREIGN KEY (dexno) REFERENCES Pokemon(dexno) ON DELETE CASCADE,
-    CONSTRAINT FOREIGN KEY (uid) REFERENCES User(uid) ON DELETE CASCADE
+    CONSTRAINT FOREIGN KEY (uid) REFERENCES Users(uid) ON DELETE CASCADE
 );
-
-#CREATE INDEX poke_index ON Pokedex (uid, dexno)
 
 CREATE TABLE Capture (
     pid INTEGER NOT NULL, #pokemon
     aid INTEGER NOT NULL, #area
     mid INTEGER NOT NULL, #method
     rid INTEGER NOT NULL, #region
+    gid INTEGER NOT NULL, #game
     tid INTEGER NOT NULL, #time
-    INDEX (pid, aid, rid),
-    PRIMARY KEY ()
+    PRIMARY KEY (pid,aid,mid,rid,gid,tid)
 );
-
-CREATE INDEX poke_capt ON Capture(pid) USING HASH;
-CREATE INDEX area_capt ON Capture(aid, rid) USING HASH;
 
 CREATE TABLE Area (
     aid INTEGER NOT NULL,
@@ -78,3 +80,29 @@ CREATE TABLE CatchMethod (
     description VARCHAR(50) NOT NULL,
     PRIMARY KEY (mid)
 );
+
+#Index Creation
+#CREATE INDEX poke_index ON Pokedex (uid, dexno);
+CREATE INDEX poke_capt ON Capture(pid, gid) USING HASH; #Includes gid for game-diffs
+CREATE INDEX area_capt ON Capture(aid, rid) USING HASH;
+
+# Listed below are some test data sets.
+
+INSERT INTO Game (gid, gen, name, plat) VALUES (1, 1, "Pokemon Red", "GameBoy");
+INSERT INTO Game (gid, gen, name, plat) VALUES (2, 1, "Pokemon Green", "GameBoy");
+INSERT INTO Game (gid, gen, name, plat) VALUES (3, 1, "Pokemon Blue", "GameBoy");
+INSERT INTO Game (gid, gen, name, plat) VALUES (4, 1, "Pokemon Yellow", "GameBoyColor");
+INSERT INTO Game (gid, gen, name, plat) VALUES (5, 2, "Pokemon Gold", "GameBoyColor");
+INSERT INTO Game (gid, gen, name, plat) VALUES (6, 2, "Pokemon Silver", "GameBoyColor");
+INSERT INTO Game (gid, gen, name, plat) VALUES (7, 2, "Pokemon Crystal", "GameBoyColor");
+
+INSERT INTO Pokemon(dexno, name, type1, type2) VALUES (1, "Bulbasaur", 1, NULL);
+INSERT INTO Pokemon(dexno, name, type1, type2) VALUES (2, "Ivysaur", 1, NULL);
+INSERT INTO Pokemon(dexno, name, type1, type2) VALUES (3, "Venasaur", 1, NULL);
+
+INSERT INTO Capture(pid, aid, mid, rid, gid, tid) VALUES (1,2,3,4,1,6);
+INSERT INTO Capture(pid, aid, mid, rid, gid, tid) VALUES (2,2,3,4,1,6);
+INSERT INTO Capture(pid, aid, mid, rid, gid, tid) VALUES (2,2,3,4,1,1);
+INSERT INTO Capture(pid, aid, mid, rid, gid, tid) VALUES (3,2,3,4,1,1);
+
+INSERT INTO Users(uid, name) VALUES (1, "test");
--- register.php
+++ register.php
@@ -30,26 +30,42 @@
 </head>
 <body>
 <center>
-    <h1>Welcome, <? $_SESSION['name'] ?>!</h1>
+    <h1>Welcome, <? $_SESSION['name']?>!</h1>
 </center>
+<!-- currently this connection is to localhost for testing purposes with dummy user.-->
+<?  $link = mysqli_connect("localhost", "test", "test", "poketool"); ?>
+
+<div id="formpage_1" style="visibility:visible; display: block">
 <h3>In what game do you want to fill the Pokédex?</h3>
 <?
-    while ($game = mysqli_fetch_array($results)) {
+    $query = "select * from Game";
+    $result = mysqli_query($link, $query);
+    while ($game = mysqli_fetch_array($result)) {
         ?>
-        <input type="radio" name="game" value="<? echo $game['id'] ?>" />
-        <? echo $game['name'] ?> (<? echo $game['system'] ?>)<br>
+        <input type="radio" name="game" value="<? echo $game['gid'] ?>" onclick=<? $selected_game=$game['gid'];?> />
+        <? echo $game['name'] ?> (<? echo $game['plat'] ?>)<br>
         <?
     }
 ?>
+<input type="submit" onclick="pagechange(1,2)">
+</div>
+<div id="formpage_2" style="visibility: hidden; display: none;">
 <h3>Which Pokémon have you caught already?</h3>
+<form action="add.php" method="post">
+<input type="hidden" name="user" value=<? echo $_SESSION['name'] ?> />
+<input type="hidden" name="game" value=<? echo $selected_game ?> />
 <?
-    while ($pokemon = mysqli_fetch_array($results)) {
+    $query = "select DISTINCT Pokemon.dexno, Pokemon.name from Pokemon JOIN Capture ON Pokemon.dexno=Capture.pid WHERE Capture.gid=1";
+    $result = mysqli_query($link, $query);
+    while ($pokemon = mysqli_fetch_array($result)) {
         ?>
-        <input type="checkbox" name="pokemon" value="<? echo $pokemon['dexno'] ?>" />
+        <input type="checkbox" name="pokemon" value=<? echo $pokemon['dexno'] ?> />
         <? echo $pokemon['dexno'] ?> - <? echo $pokemon['name'] ?><br>
         <?
     }
 ?>
+<input type="submit"></form>
+</div>
 
 <div class="footer">
     <a href="https://github.com/RenanGreca/poke-tool">Created by Renan Greca and Mari Bennett in 2014</a><br />
