76d011b1063aec6b519aa923ae91c21d6afc58c3
Mari Bennett <bennett.mari@gmail.com>
2014-05-04 16:03:03 -0700

    Created multi-option main page (currently pokedex and to-find-list);

--- main.php
+++ main.php
@@ -2,7 +2,8 @@
 
 
 if (isset($_POST)){
- $selected_game = $_POST['game'];}
+ $selected_game = $_POST['game'];
+ $task = $_POST['task'];}
 ?>
 
 <!DOCTYPE html>
@@ -22,31 +23,48 @@ if (isset($_POST)){
 <?  $link = mysqli_connect("localhost", "test", "test", "poketool"); 
 if(!$selected_game){ ?>
 <h3>What would you like to do?</h3>
-    <?
-    $query = "select * from Game";
-    $result = mysqli_query($link, $query); ?>
     <form action="#" method="POST">
     <select name="game"> 
     <option value="">Select Game..</option> 
    <?
+    $query = "select * from Game";
+    $result = mysqli_query($link, $query);
     while ($game = mysqli_fetch_array($result)) {
         ?>
         <option value="<? echo $game['gid'] ?>"><? echo $game['name']?> </option></br>
         <?
     }
     ?></select>
-<input type="submit" value="See My Pokedex"></form>
-<? } else { ?>
+    <select name="task"> 
+    <option value="">....</option> 
+    <option value="pokedex">See my pokedex</option>
+    <option value="find">Help me find pokemon</option>
+    </select>
+<input type="submit" value="Go!"></form>
+
+<? } elseif($task == "pokedex"){ 
+$query = "select * from Pokemon JOIN Pokedex ON Pokemon.dexno = Pokedex.dexno WHERE Pokedex.game='$selected_game'";
+?>
 <div>
 <?
-    $query = "select * from Pokemon JOIN Pokedex ON Pokemon.dexno = Pokedex.dexno WHERE Pokedex.game='$selected_game'";
     $result = mysqli_query($link, $query); 
     while ($pokemon = mysqli_fetch_array($result)) {
          echo $pokemon['name']; ?> <br>
         <?
-    }
-}?>
+    } ?>
 </div>
+  <? } elseif($task == "find") { ?>
+<div>
+<?
+    $query = "select * from Pokemon JOIN Capture ON Pokemon.dexno = Capture.pid WHERE Capture.gid = '$selected_game' AND Capture.pid NOT IN (SELECT dexno FROM Pokedex WHERE uid=1)";
+    $result = mysqli_query($link, $query); 
+    while ($pokemon = mysqli_fetch_array($result)) {
+         echo $pokemon['name']; ?> <br>
+        <?
+    } ?></div><?
+} 
+else { Echo $_POST['task']; }
+?>
 <div class="footer">
     <a href="https://github.com/RenanGreca/poke-tool">Created by Renan Greca and Mari Bennett in 2014</a><br />
     Pokémon and all related content is owned by The Pokémon Company, Nintendo and GameFreak<br />
